0dd7a867a8aa8f56e04fabd88f9fbe3fa21fb797
Fumiyori Manaka <fumi.manaka.0213@gmail.com>
2026-01-20 18:35:16 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2026 Fumiyori Manaka
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng (){
+	echo NG at line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
